f6e0a733fd22f5447329da8f93cd6d623971f9be
Mohammad Zaif Quraishi <mzaifquraishi@users.noreply.github.com>
2021-07-14 21:25:13 +0530

    redux correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useEffect } from 'react';
 import './App.css';
-import { Ticker } from './ticker/Ticker';
-
+import { connect } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import Ticker from './ticker/Ticker';
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    let timer1 = setTimeout(() => fetch('https://api-pub.bitfinex.com/v2/tickers?symbols=ALL').then(d => d.json()).then(d => {
+      const dd = d.map((j: any) => [j[0].toString().substr(1, 3),
+      j[0].toString().substr(4),
+      j[1],
+      j[8],
+      j[9],
+      j[10],
+      j[0]])
+      dispatch({ type: 'SET_DATA', data: dd });
+    }), 1 * 1000);
+    return () => {
+      clearTimeout(timer1);
+    };
+  });
   return (
     <div className="App">
       <header className="container">
-      <div className="jumbotron">
-      <h1 className="display-4">Ticker</h1></div>
+        <div className="jumbotron">
+          <h1 className="display-4">Ticker</h1></div>
         <Ticker />
       </header>
     </div>
   );
 }
 
-export default App;
+export default connect()(App);
--- src/ticker/Ticker.tsx
+++ src/ticker/Ticker.tsx
@@ -1,37 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 
-import { useAppSelector, useAppDispatch } from '../app/hooks';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from '../features/counter/counterSlice';
-import styles from '../features/counter/Counter.module.css';
 
-export function Ticker(): any {
+function Ticker(props: any): any {
   const [data, setdata] = useState([]);
-
   useEffect(() => {
-    let timer1 = setTimeout(() => fetch('https://api-pub.bitfinex.com/v2/tickers?symbols=ALL').then(d => d.json()).then(d => {
-      const dd = d.map((j: any) => [j[0].toString().substr(1, 3),
-      j[0].toString().substr(4),
-      j[1],
-      j[8],
-      j[9],
-      j[10],
-      j[0]])
-      setdata(dd);
-      console.log(d);
-    }), 2 * 1000);
-    
-    return () => {
-      clearTimeout(timer1);
-    };
-  }, [data]);
-
+    let d = setTimeout(() => props.data.length && setdata(props.data), 1000)
+  })
   return (
     <div className="table-responsive">
       <table className="table table-stripe text-center table-hover">
@@ -59,3 +34,10 @@ export function Ticker(): any {
     </div>
   );
 }
+
+const mapStateToProps = (state: any) => {
+  return {
+    data: state
+  };
+};
+export default connect(mapStateToProps)(Ticker);
